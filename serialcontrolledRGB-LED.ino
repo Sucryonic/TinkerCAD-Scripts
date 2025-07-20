8e6837dce96580db035ee1f784ac8aeb4cd4aa1e
Steven Lin <156337354+steven-llin@users.noreply.github.com>
2025-07-20 18:05:41 -0500

    Create serialcontrolledRGB-LED.ino

---
+++ serialcontrolledRGB-LED.ino
@@ -0,0 +1,53 @@
+void setup()
+{
+  for(int i = 13; i > 10; i--)
+  {
+    pinMode(i, OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+bool answer;
+int r;
+int g;
+int b;
+
+void loop()
+{
+  Serial.println("R(0-255): ");
+  answer = false;
+  while(answer == false)
+  {
+   	if(Serial.available() > 0)
+    {
+      r = Serial.parseInt(); 
+      answer = true;
+    }
+  }
+  
+  Serial.println("G(0-255): ");
+  answer = false;
+  while(answer == false)
+  {
+   	if(Serial.available() > 0)
+    {
+      g = Serial.parseInt(); 
+      answer = true;
+    }
+  }
+  
+  Serial.println("B(0-255): ");
+  answer = false;
+  while(answer == false)
+  {
+   	if(Serial.available() > 0)
+    {
+      b = Serial.parseInt(); 
+      answer = true;
+    }
+  }
+  
+  analogWrite(13, r);
+  analogWrite(12, b);
+  analogWrite(11, g);
+}
