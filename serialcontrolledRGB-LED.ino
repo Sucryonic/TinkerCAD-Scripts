4706ad62a0391321d28cedb8787891a3c57d7eb4
Steven Lin <156337354+steven-llin@users.noreply.github.com>
2025-07-20 18:31:58 -0500

    Update serialcontrolledRGB-LED.ino

--- serialcontrolledRGB-LED.ino
+++ serialcontrolledRGB-LED.ino
@@ -7,6 +7,7 @@ void setup()
   Serial.begin(9600);
 }
 
+bool answer;
 int r;
 int g;
 int b;
@@ -14,35 +15,42 @@ int b;
 void loop()
 {
   Serial.println("R(0-255): ");
-  while(Serial.available() == 0)
+  answer = false;
+  while(answer == false)
   {
    	if(Serial.available() > 0)
     {
       r = Serial.parseInt(); 
+      answer = true;
     }
   }
   
   Serial.println("G(0-255): ");
-  while(Serial.available() == 0)
+  answer = false;
+  while(answer == false)
   {
    	if(Serial.available() > 0)
     {
       g = Serial.parseInt(); 
+      answer = true;
     }
   }
   
   Serial.println("B(0-255): ");
-  while(Serial.available() == 0)
+  answer = false;
+  while(answer == false)
   {
    	if(Serial.available() > 0)
     {
       b = Serial.parseInt(); 
+      answer = true;
     }
   }
   
   analogWrite(13, r);
   analogWrite(12, b);
   analogWrite(11, g);
-  Serial.println("(" + String(r) + ", " + String(g) + ", " + String(b) + ")");
-  Serial.println("Try Again!");
+  Serial.println("RGB:(" + String(r) + ", " + String(g) + ", " + String(b) + ")");
+  Serial.println("Try Again");
+  
 }
