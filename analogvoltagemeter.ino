6de4443050c23da275c10dd44145fb3ca4ad1320
Steven Lin <156337354+Sucryonic@users.noreply.github.com>
2025-07-19 20:48:44 -0500

    Update analogvoltagemeter.ino

--- analogvoltagemeter.ino
+++ analogvoltagemeter.ino
@@ -11,20 +11,27 @@ void setup()
 void loop()
 {
   int sensorValue = analogRead(A0); 
-  if(sensorValue <= 341)
+  while(sensorValue != 0)
   {
-  	digitalWrite(12, HIGH);
-    delay(100);
-  }
-  else if(sensorValue <= 682 && sensorValue > 341)
-  {
-    digitalWrite(10, HIGH);
-    delay(100);
-  }
-  else if(sensorValue <= 1023 && sensorValue > 682);
-  {
-    digitalWrite(11, HIGH);
-    delay(100);
+    if(sensorValue <= 341)
+    {
+      delay(10);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+    }else if(sensorValue <= 682 && sensorValue > 341)
+      {
+        delay(10);
+        digitalWrite(10, HIGH);
+        digitalWrite(11, LOW);
+        digitalWrite(12, LOW);
+      }else if(sensorValue <= 1023 && sensorValue > 682);
+        {
+          delay(10);
+          digitalWrite(11, HIGH);
+          digitalWrite(10, LOW);
+          digitalWrite(12, LOW);
+        }
   }
   Serial.println(sensorValue);
 }
